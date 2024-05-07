6834ec81c94402e0ac7e7da4d61740a464e4ac28
omar adel <elhwety5555@gmail.com>
2024-05-07 18:04:40 +0300

    implement search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ProcessComponent } from './process/process.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ProcessComponent],
 })
 export class AppComponent {
   title = 'erp-a-technology';
