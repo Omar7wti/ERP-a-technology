9563b366389e0be3b92df4855e657665ff404cda
Omar <elhwety5555@gmail.com>
2024-05-08 20:10:01 +0200

    fix ui

--- src/app/process/process.component.ts
+++ src/app/process/process.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FilterComponent } from '../filter/filter.component';
 import { SearchComponent } from '../search/search.component';
+import { RangeComponent } from '../range/range.component';
 
 @Component({
   selector: 'app-process',
   standalone: true,
-  imports: [FilterComponent, SearchComponent],
+  imports: [FilterComponent, SearchComponent, RangeComponent],
   templateUrl: './process.component.html',
   styleUrl: './process.component.css',
 })
