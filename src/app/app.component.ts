d132e03fc1585d99a2557e3f2cfef2c6622fae02
omar adel <elhwety5555@gmail.com>
2024-05-09 20:08:11 +0300

    implement footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ProcessComponent } from './process/process.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, ProcessComponent],
+  imports: [RouterOutlet, HeaderComponent, ProcessComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'erp-a-technology';
