f701ebf55702e5a9020985ed8066cb97b7d6cd01
omar adel <elhwety5555@gmail.com>
2024-05-09 17:19:23 +0300

    implement marks section

--- src/app/process/process.component.ts
+++ src/app/process/process.component.ts
@@ -3,12 +3,19 @@ import { FilterComponent } from '../filter/filter.component';
 import { SearchComponent } from '../search/search.component';
 import { RangeComponent } from '../range/range.component';
 import { DetailsComponent } from '../details/details.component';
+import { MarksComponent } from '../marks/marks.component';
 
 @Component({
   selector: 'app-process',
   standalone: true,
   templateUrl: './process.component.html',
   styleUrl: './process.component.css',
-  imports: [FilterComponent, SearchComponent, RangeComponent, DetailsComponent],
+  imports: [
+    FilterComponent,
+    SearchComponent,
+    RangeComponent,
+    DetailsComponent,
+    MarksComponent,
+  ],
 })
 export class ProcessComponent {}
