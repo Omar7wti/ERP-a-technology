be038d38b9f5f993dab8ef6c144e1afe001d2c9a
omar adel <elhwety5555@gmail.com>
2024-05-09 16:31:52 +0300

    implement details section

--- src/app/process/process.component.ts
+++ src/app/process/process.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { FilterComponent } from '../filter/filter.component';
 import { SearchComponent } from '../search/search.component';
 import { RangeComponent } from '../range/range.component';
+import { DetailsComponent } from '../details/details.component';
 
 @Component({
   selector: 'app-process',
   standalone: true,
-  imports: [FilterComponent, SearchComponent, RangeComponent],
   templateUrl: './process.component.html',
   styleUrl: './process.component.css',
+  imports: [FilterComponent, SearchComponent, RangeComponent, DetailsComponent],
 })
 export class ProcessComponent {}
